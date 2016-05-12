7805d26584e74735076ca53050897d2768580433
Adam Morgan <aymorgan@hotmail.co.uk>
2016-05-12 17:08:55 +0100

    Farz shiz3

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ app.post('/webhook/', function (req, res) {
     for (i = 0; i < messaging_events.length; i++) {
         event = req.body.entry[0].messaging[i];
         sender = event.sender.id;
+								console.log('SENDER' , sender);
         if (event.message && event.message.text) {
             text = event.message.text;
             sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
@@ -45,55 +46,6 @@ var token = "EAAHDcdvZAfTABALAvgjUaFSgO5UrBIDemxPZCOHFhc7lj5eVrnJBx10R4SevW4ZCCE
 function sendTextMessage(sender, text) {
 	messageData = {
 		text:text
-	}
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
-	})
-}
-
-function sendGenericMessage(sender) {
-	messageData = {
-		"attachment": {
-			"type": "template",
-			"payload": {
-				"template_type": "generic",
-				"elements": [{
-					"title": "First card",
-					"subtitle": "Element #1 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-					"buttons": [{
-						"type": "web_url",
-						"url": "https://www.messenger.com",
-						"title": "web url"
-					}, {
-						"type": "postback",
-						"title": "Postback",
-						"payload": "Payload for first element in a generic bubble",
-					}],
-				}, {
-					"title": "Second card",
-					"subtitle": "Element #2 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-					"buttons": [{
-						"type": "postback",
-						"title": "Postback",
-						"payload": "Payload for second element in a generic bubble",
-					}],
-				}]
-			}
-		}
 	}
 	request({
 		url: 'https://graph.facebook.com/v2.6/me/messages',
