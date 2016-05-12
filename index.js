10197ba291451ffc3c4e97f3dff374e58b39594f
Adam Morgan <aymorgan@hotmail.co.uk>
2016-05-12 16:43:26 +0100

    Farz shiz

--- index.js
+++ index.js
@@ -26,25 +26,16 @@ app.get('/webhook/', function (req, res) {
 
 // to post data
 app.post('/webhook/', function (req, res) {
-	messaging_events = req.body.entry[0].messaging
-	for (i = 0; i < messaging_events.length; i++) {
-		event = req.body.entry[0].messaging[i]
-		sender = event.sender.id
-		if (event.message && event.message.text) {
-			text = event.message.text
-			if (text === 'Generic') {
-				sendGenericMessage(sender)
-				continue
-			}
-			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-		}
-		if (event.postback) {
-			text = JSON.stringify(event.postback)
-			sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
-			continue
-		}
-	}
-	res.sendStatus(200)
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
 })
 
 var token = "EAAHDcdvZAfTABALAvgjUaFSgO5UrBIDemxPZCOHFhc7lj5eVrnJBx10R4SevW4ZCCEzswQ0hzWGZBPonGeLPUwX1PUlZB2JkuxhQZC1uzMZAp9wObZBZBUBysMHtQDvDBCqXI03IsLzgULqkZBJIJsZCzedwbp56JYOUknyGcZBHeps6WgZDZD"
